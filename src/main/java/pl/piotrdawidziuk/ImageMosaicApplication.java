eb8f1ec6714afd64aaabfad34bb55401c33e367d
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-16 13:23:03 +0200

    first commit

---
+++ src/main/java/pl/piotrdawidziuk/ImageMosaicApplication.java
@@ -0,0 +1,13 @@
+package pl.piotrdawidziuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageMosaicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageMosaicApplication.class, args);
+	}
+
+}
